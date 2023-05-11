ee34aba169e17821619cf24e9d6d3fa286d37b06
Natalia Graczyk <natka.graczyk@gmail.com>
2023-05-11 17:55:15 +0200

    backend init

---
+++ backend/backend-movie-app/src/test/java/pl/watchme/backendmovieapp/BackendMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.watchme.backendmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
